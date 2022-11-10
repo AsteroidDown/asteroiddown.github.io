b85f13b7560b81b49fac306a9eccc5e784c577df
AsteroidDown <jasper@stahmer.ca>
2022-11-10 07:38:13 -0500

    Start to angular project

---
+++ src/libs/components/src/lib/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { WelcomeComponent } from "./components/welcome/welome.component";
+
+@NgModule({
+  declarations: [WelcomeComponent],
+  imports: [],
+  exports: [WelcomeComponent],
+})
+export class ComponentsModule {}
